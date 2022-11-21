316b820f0f4a08691ddb762c0e0dd8628d147aa3
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-21 13:10:22 +0900

    Userモデルにバリデーション追加

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :items
+
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true
+  validates :first_name,       presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana,  presence: true
+  validates :birth_date,       presence: true
+
+end
