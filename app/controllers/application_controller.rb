c6024abc8712cb65c0eab9cf458ddc5bc4ddb604
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-21 13:11:38 +0900

    ヘッダーの表示をログインの有無で分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :questions, only: [:index]
+
+  resources :items, only: [:index]
+
+resources :users, only: :show
+
 end
