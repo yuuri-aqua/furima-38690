de5efb6823de5eb5223eea47189b4b22284989e5
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-21 15:51:24 +0900

    DB名を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_030357) do
+ActiveRecord::Schema.define(version: 2022_11_21_063613) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
