62756b8022f87273cb4e910651186b610adedb2f
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-21 17:09:50 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,2 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
-  def index
-    @items = Item.all
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    Item.create(item_params)
-  end
-
-  def show
-  end
-
-  private
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:index]
-
-resources :users, only: :show
+  resources :items, only: [:index, :create, :destroy]
 
 end
