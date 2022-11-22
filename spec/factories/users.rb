dd349e78da795bfec1fa623fb5ce15af602df93e
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-22 14:52:24 +0900

    Userモデルの単体テストコード作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,13 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
+    family_name           {'名字'}
+    first_name            {'名前'}
+    family_name_kana      {'ミョウジ'}
+    first_name_kana       {'ナマエ'}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+
   end
 end
