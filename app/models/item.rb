eef146daa5e57ad605d8a94da1ba28cfcccb9fb4
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-11-23 12:01:23 +0900

    出品商品の登録準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :product_name,         presence: true
+  validates :product_description,  presence: true
+  validates :product_category_id,  presence: true
+  validates :product_condition_id, presence: true
+  validates :shipping_charge_id,   presence: true
+  validates :prefecture_id,        presence: true
+  validates :days_to_ship_id,      presence: true
+  validates :selling_price,        presence: true
+
+  validates :image, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :items
+
   validates :nickname,         presence: true
   validates :family_name,      presence: true
   validates :first_name,       presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana,  presence: true
   validates :birth_date,       presence: true
-
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'}
